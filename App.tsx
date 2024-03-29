2107519d272fcc494cb01a93118caf6667494476
DevanshSK <devanshs843@gmail.com>
2024-03-30 01:16:41 +0530

    Day-1: Finished added custom font with splash screen.

--- App.tsx
+++ App.tsx
@@ -2,17 +2,37 @@ import { StatusBar } from 'expo-status-bar';
 import { FlatList, StyleSheet, Text, View, SafeAreaView, ActivityIndicator } from 'react-native';
 import DayListItem from './src/components/core/DayListItem';
 
+// Font imports
 import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
+import {AmaticSC_400Regular, AmaticSC_700Bold} from "@expo-google-fonts/amatic-sc";
 
+// Splash Screen Imports
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+// Dynamic data for out mini projects.
 const days = [...Array(24)].map((val, index) => index + 1);
 
 export default function App() {
   const [fontsLoaded, fontError] = useFonts({
     InterBlack: Inter_900Black,
+    Amatic: AmaticSC_400Regular,
+    AmaticBold: AmaticSC_700Bold
   });
 
-  if(!fontsLoaded && !fontError){
-    return <ActivityIndicator />
+
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
   }
 
   return (
@@ -44,5 +64,5 @@ const styles = StyleSheet.create({
   column: {
     gap: 10
   },
-  
+
 });
